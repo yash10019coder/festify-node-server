26f79895910558efc9e1d1944e98dd8f0eb376a6
yash10019coder <lit2020066@iiitl.ac.in>
2022-01-06 16:12:22 +0530

    added jwt

--- routes/dist/user.dev.js
+++ routes/dist/user.dev.js
@@ -6,7 +6,13 @@ var router = express.Router();
 
 var User = require("../models/user");
 
-router.get("/:name", function (req, res) {
+var jwt = require("jsonwebtoken");
+
+var verify = require("./verify_token");
+
+var fs = require('fs');
+
+router.get("/:name", verify, function (req, res) {
   try {
     User.findOne({
       userName: req.params.name
@@ -91,10 +97,21 @@ router.post("/login", function _callee(req, res) {
               status: 0,
               message: "User not found"
             });
-          } else res.status(200).json({
-            status: 1,
-            message: "User is logged in",
-            token: token
+          } else jwt.sign({
+            user: user
+          }, process.env.TOKEN_SECRET, function (err, token) {
+            if (err) {
+              console.error(err);
+              res.status(400).json({
+                status: 0,
+                message: err
+              });
+            }
+
+            res.header('auth-token', token).status(200).json({
+              status: 1,
+              message: "User is logged in"
+            });
           });
 
           _context.next = 23;
@@ -117,7 +134,7 @@ router.post("/login", function _callee(req, res) {
   }, null, null, [[0, 19]]);
 });
 router.post("/create", function _callee2(req, res) {
-  var a1, a2;
+  var a1, a2, base64Data;
   return regeneratorRuntime.async(function _callee2$(_context2) {
     while (1) {
       switch (_context2.prev = _context2.next) {
@@ -138,39 +155,65 @@ router.post("/create", function _callee2(req, res) {
         case 6:
           a2 = _context2.sent;
 
-          if (a1 !== null) {
-            res.status(400).json({
-              status: 0,
-              message: "User alredy exists please use a different email"
-            });
-          } else if (a2 !== null) {
-            res.status(400).json({
-              status: 0,
-              message: "User alredy exists please use a different username"
-            });
-          } else {
-            User.create(req.body, function (err, user) {
-              if (err) {
-                console.error(err);
-                res.status(400).json({
-                  status: 0,
-                  message: err
-                });
-              }
+          if (!(a1 !== null)) {
+            _context2.next = 11;
+            break;
+          }
 
-              console.log(user);
-              res.status(200).json({
-                status: 1,
-                message: "user is succesfully created "
-              });
+          res.status(400).json({
+            status: 0,
+            message: "User alredy exists please use a different email"
+          });
+          _context2.next = 19;
+          break;
+
+        case 11:
+          if (!(a2 !== null)) {
+            _context2.next = 15;
+            break;
+          }
+
+          res.status(400).json({
+            status: 0,
+            message: "User alredy exists please use a different username"
+          });
+          _context2.next = 19;
+          break;
+
+        case 15:
+          base64Data = req.body.userPhoto.replace(/^data:image\/png;base64,/, "");
+
+          if (!fs.existsSync('../images')) {
+            fs.mkdirSync('../images');
+          } else {
+            fs.writeFile("".concat(req.body.userName, ".png"), base64Data, 'base64', function (err) {
+              console.log(err);
             });
           }
 
-          _context2.next = 14;
+          _context2.next = 19;
+          return regeneratorRuntime.awrap(User.create(req.body, function (err, user) {
+            if (err) {
+              console.error(err);
+              res.status(400).json({
+                status: 0,
+                message: err
+              });
+            }
+
+            console.log(user);
+            res.status(200).json({
+              status: 1,
+              message: "user is successfully created "
+            });
+          }));
+
+        case 19:
+          _context2.next = 25;
           break;
 
-        case 10:
-          _context2.prev = 10;
+        case 21:
+          _context2.prev = 21;
           _context2.t0 = _context2["catch"](0);
           console.error(_context2.t0);
           res.status(400).json({
@@ -178,11 +221,11 @@ router.post("/create", function _callee2(req, res) {
             message: _context2.t0
           });
 
-        case 14:
+        case 25:
         case "end":
           return _context2.stop();
       }
     }
-  }, null, null, [[0, 10]]);
+  }, null, null, [[0, 21]]);
 });
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,53 +1,61 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 const verify = require("./verify_token");
+const fs = require('fs');
 
 router.get("/:name", verify, (req, res) => {
     try {
-        User.findOne({userName: req.params.name}, (err, user) => {
+        User.findOne({ userName: req.params.name }, (err, user) => {
             if (err) {
                 console.error(err);
-                res.status(400).json({status: 0, message: err});
+                res.status(400).json({ status: 0, message: err });
             }
             console.log(user);
             if (user === null) {
-                res.status(404).json({status: 0, message: "User not found"});
+                res.status(404).json({ status: 0, message: "User not found" });
             } else {
-                res.status(200).json({status: 1, data: user});
+                res.status(200).json({ status: 1, data: user });
             }
         });
     } catch (error) {
         console.error(error);
-        res.status(400).json({status: 0, message: error});
+        res.status(400).json({ status: 0, message: error });
     }
 });
 
-router.post("/login", verify, async (req, res) => {
+router.post("/login", async (req, res) => {
     try {
         let usernameOrEmail = await req.body.usernameOrEmail;
         let password = await req.body.password;
         let user;
         if (usernameOrEmail.includes("@")) {
-            user = await User.findOne({userEmail: usernameOrEmail, userPassword: password});
+            user = await User.findOne({ userEmail: usernameOrEmail, userPassword: password });
         } else {
-            user = await User.findOne({userName: usernameOrEmail, userPassword: password});
+            user = await User.findOne({ userName: usernameOrEmail, userPassword: password });
         }
         if (user === null) {
-            res.status(404).json({status: 0, message: "User not found"});
+            res.status(404).json({ status: 0, message: "User not found" });
         } else
-            res.status(200).json({status: 1, message: "User is logged in", token});
+            jwt.sign({ user }, process.env.TOKEN_SECRET, (err, token) => {
+                if (err) {
+                    console.error(err);
+                    res.status(400).json({ status: 0, message: err });
+                }
+                res.header('auth-token', token).status(200).json({ status: 1, message: "User is logged in" });
+            });
     } catch (error) {
         console.error(error);
-        res.status(400).json({status: 0, message: error});
+        res.status(400).json({ status: 0, message: error });
     }
 });
 
-router.post("/create", verify, async (req, res) => {
+router.post("/create", async (req, res) => {
     try {
         let a1, a2;
-        a1 = await User.findOne({userEmail: req.body.userEmail});
-        a2 = await User.findOne({userName: req.body.userName});
+        a1 = await User.findOne({ userEmail: req.body.userEmail });
+        a2 = await User.findOne({ userName: req.body.userName });
 
         if (a1 !== null) {
             res.status(400).json({
@@ -60,18 +68,27 @@ router.post("/create", verify, async (req, res) => {
                 message: "User alredy exists please use a different username",
             });
         } else {
-            User.create(req.body, (err, user) => {
+            let base64Data = req.body.userPhoto.replace(/^data:image\/png;base64,/, "");
+
+            if (!fs.existsSync('../images')) {
+                fs.mkdirSync('../images');
+            } else {
+                fs.writeFile(`${req.body.userName}.png`, base64Data, 'base64', function (err) {
+                    console.log(err);
+                });
+            }
+            await User.create(req.body, (err, user) => {
                 if (err) {
                     console.error(err);
-                    res.status(400).json({status: 0, message: err});
+                    res.status(400).json({ status: 0, message: err });
                 }
                 console.log(user);
-                res.status(200).json({status: 1, message: "user is succesfully created "});
+                res.status(200).json({ status: 1, message: "user is successfully created " });
             });
         }
     } catch (error) {
         console.error(error);
-        res.status(400).json({status: 0, message: error});
+        res.status(400).json({ status: 0, message: error });
     }
 });
 
