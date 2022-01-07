fab61836d153022b148dbdbe7c26f5ed7f9cb904
yash10019coder <lit2020066@iiitl.ac.in>
2022-01-07 12:10:25 +0530

    added image support

--- routes/user.js
+++ routes/user.js
@@ -7,21 +7,21 @@ const fs = require('fs');
 
 router.get("/:name", verify, (req, res) => {
     try {
-        User.findOne({ userName: req.params.name }, (err, user) => {
+        User.findOne({userName: req.params.name}, (err, user) => {
             if (err) {
                 console.error(err);
-                res.status(400).json({ status: 0, message: err });
+                res.status(400).json({status: 0, message: err});
             }
             console.log(user);
             if (user === null) {
-                res.status(404).json({ status: 0, message: "User not found" });
+                res.status(404).json({status: 0, message: "User not found"});
             } else {
-                res.status(200).json({ status: 1, data: user });
+                res.status(200).json({status: 1, data: user});
             }
         });
     } catch (error) {
         console.error(error);
-        res.status(400).json({ status: 0, message: error });
+        res.status(400).json({status: 0, message: error});
     }
 });
 
@@ -31,31 +31,31 @@ router.post("/login", async (req, res) => {
         let password = await req.body.password;
         let user;
         if (usernameOrEmail.includes("@")) {
-            user = await User.findOne({ userEmail: usernameOrEmail, userPassword: password });
+            user = await User.findOne({userEmail: usernameOrEmail, userPassword: password});
         } else {
-            user = await User.findOne({ userName: usernameOrEmail, userPassword: password });
+            user = await User.findOne({userName: usernameOrEmail, userPassword: password});
         }
         if (user === null) {
-            res.status(404).json({ status: 0, message: "User not found" });
+            res.status(404).json({status: 0, message: "User not found"});
         } else
-            jwt.sign({ user }, process.env.TOKEN_SECRET, (err, token) => {
+            jwt.sign({user}, process.env.TOKEN_SECRET, (err, token) => {
                 if (err) {
                     console.error(err);
-                    res.status(400).json({ status: 0, message: err });
+                    res.status(400).json({status: 0, message: err});
                 }
-                res.header('auth-token', token).status(200).json({ status: 1, message: "User is logged in" });
+                res.header('auth-token', token).status(200).json({status: 1, message: "User is logged in"});
             });
     } catch (error) {
         console.error(error);
-        res.status(400).json({ status: 0, message: error });
+        res.status(400).json({status: 0, message: error});
     }
 });
 
 router.post("/create", async (req, res) => {
     try {
         let a1, a2;
-        a1 = await User.findOne({ userEmail: req.body.userEmail });
-        a2 = await User.findOne({ userName: req.body.userName });
+        a1 = await User.findOne({userEmail: req.body.userEmail});
+        a2 = await User.findOne({userName: req.body.userName});
 
         if (a1 !== null) {
             res.status(400).json({
@@ -73,22 +73,23 @@ router.post("/create", async (req, res) => {
             if (!fs.existsSync('../images')) {
                 fs.mkdirSync('../images');
             } else {
-                fs.writeFile(`${req.body.userName}.png`, base64Data, 'base64', function (err) {
+                fs.writeFile(`../images/${req.body.userName}.png`, base64Data, 'base64', function (err) {
                     console.log(err);
                 });
             }
+            req.body.userPhoto = `https://festify-iiitl.herokuapp.com/images/${req.body.userName}.png`;
             await User.create(req.body, (err, user) => {
                 if (err) {
                     console.error(err);
-                    res.status(400).json({ status: 0, message: err });
+                    res.status(400).json({status: 0, message: err});
                 }
                 console.log(user);
-                res.status(200).json({ status: 1, message: "user is successfully created " });
+                res.status(200).json({status: 1, message: "user is successfully created "});
             });
         }
     } catch (error) {
         console.error(error);
-        res.status(400).json({ status: 0, message: error });
+        res.status(400).json({status: 0, message: error});
     }
 });
 
