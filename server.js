a33ce4ae7d432ffb2a997bf50b90c924e1e35070
yash10019coder <lit2020066@iiitl.ac.in>
2022-01-07 12:54:02 +0530

    bug fixes.

--- server.js
+++ server.js
@@ -12,25 +12,27 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 8080;
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, () => {
-  console.log("Connected to MongoDB");
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true}, () => {
+    console.log("Connected to MongoDB");
 });
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(express.static("./images"))
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
 app.use('/event', event);
 app.use('/user', user)
 
 app.get("/", (req, res) => {
-  console.log(req.url);
-  console.log(req.headers);
-  console.log(req.body);
-  console.log(req.ip);
-  res.send(req.headers, req.data, req.ip, req.router, req.body);
+    console.log(req.url);
+    console.log(req.headers);
+    console.log(req.body);
+    console.log(req.ip);
+    res.send(req.headers, req.data, req.ip, req.router, req.body);
 });
 
 app.listen(process.env.PORT || port, () => {
-  console.log(`Server running at http://${ip.address()}:${port}`);
+    console.log(`Server running at http://${ip.address()}:${port}`);
 });
 
 module.exports = app;
