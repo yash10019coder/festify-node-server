293cd2b4c26fa82f6c8542d6a39a4b4f10c43912
Rohit1173 <lcs2020048@iiitl.ac.in>
2022-05-30 01:31:02 +0530

    Revert "event"
    This reverts commit 54085e327943abfcb6584ca98f1005d4728a6031.

--- models/dist/event.dev.js
+++ models/dist/event.dev.js
@@ -5,35 +5,27 @@ var mongoose = require("mongoose");
 var eventSchema = mongoose.Schema({
   eventName: {
     type: String,
-    required: true,
-  },
-  eventCategory: {
-    type: String,
-    required: true,
+    required: true
   },
   eventDate: {
     type: String,
-    required: true,
+    required: true
   },
-  eventEndDate: {
+  eventTime: {
     type: String,
-    required: true,
+    required: true
   },
-  eventStartTime: {
+  eventLocation: {
     type: String,
-    required: true,
-  },
-  eventEndTime: {
-    type: String,
-    required: true,
+    required: true
   },
   eventDescription: {
     type: String,
-    required: true,
+    required: true
   },
   eventImage: {
     type: String,
-    required: true,
-  },
+    required: true
+  }
 });
 module.exports = mongoose.model("Event", eventSchema);
