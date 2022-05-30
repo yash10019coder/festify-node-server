d0fdf276f3601eaf30f8d8c692c9d1217477f03d
yash10019coder <yashverma10019@gmail.com>
2022-05-30 12:57:12 +0530

    added some new fields in data model of events

--- models/dist/event.dev.js
+++ models/dist/event.dev.js
@@ -1,9 +1,17 @@
 "use strict";
 
+var _mongoose$Schema;
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var mongoose = require("mongoose");
 
-var eventSchema = mongoose.Schema({
+var eventSchema = mongoose.Schema((_mongoose$Schema = {
   eventName: {
+    type: String,
+    required: true
+  },
+  eventCategory: {
     type: String,
     required: true
   },
@@ -11,11 +19,15 @@ var eventSchema = mongoose.Schema({
     type: String,
     required: true
   },
-  eventTime: {
+  eventEndDate: {
     type: String,
     required: true
   },
-  eventLocation: {
+  eventStartTime: {
+    type: String,
+    required: true
+  },
+  eventEndTime: {
     type: String,
     required: true
   },
@@ -27,5 +39,23 @@ var eventSchema = mongoose.Schema({
     type: String,
     required: true
   }
-});
+}, _defineProperty(_mongoose$Schema, "eventName", {
+  type: String,
+  required: true
+}), _defineProperty(_mongoose$Schema, "eventDate", {
+  type: String,
+  required: true
+}), _defineProperty(_mongoose$Schema, "eventTime", {
+  type: String,
+  required: true
+}), _defineProperty(_mongoose$Schema, "eventLocation", {
+  type: String,
+  required: true
+}), _defineProperty(_mongoose$Schema, "eventDescription", {
+  type: String,
+  required: true
+}), _defineProperty(_mongoose$Schema, "eventImage", {
+  type: String,
+  required: true
+}), _mongoose$Schema));
 module.exports = mongoose.model("Event", eventSchema);
--- models/event.js
+++ models/event.js
@@ -5,6 +5,37 @@ var eventSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  eventCategory: {
+    type: String,
+    required: true,
+  },
+  eventDate: {
+    type: String,
+    required: true,
+  },
+  eventEndDate: {
+    type: String,
+    required: true,
+  },
+  eventStartTime: {
+    type: String,
+    required: true,
+  },
+  eventEndTime: {
+    type: String,
+    required: true,
+  },
+  eventDescription: {
+    type: String,
+    required: true,
+  },
+  eventImage: {
+    type: String,
+    required: true,
+  },eventName: {
+    type: String,
+    required: true,
+  },
   eventDate: {
     type: String,
     required: true,
