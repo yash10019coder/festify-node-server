f9b0e3635508842aa24f3ca63fd98ef148175cb5
yash10019coder <yashverma10019@gmail.com>
2022-10-06 14:35:36 +0530

    added a demi endpoint for Nuclear Detection Project

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -10,7 +10,11 @@ var event = require("./routes/event");
 
 var user = require("./routes/user");
 
+var verify = require("./routes/verify");
+
 var ip = require("ip");
+
+require("dotenv").config();
 
 require("dotenv/config");
 
@@ -23,22 +27,36 @@ mongoose.connect(process.env.DB_URL, {
 }, function () {
   console.log("Connected to MongoDB");
 });
+app.use(express["static"]("./images"));
 app.use(bodyParser.json({
-  limit: '50mb'
+  limit: "50mb"
 }));
 app.use(bodyParser.urlencoded({
-  limit: '50mb',
+  limit: "50mb",
   extended: true
 }));
 app.use(cors());
-app.use('/event', event);
-app.use('/user', user);
+app.use("/event", event);
+app.use("/user", user);
 app.get("/", function (req, res) {
   console.log(req.url);
   console.log(req.headers);
   console.log(req.body);
   console.log(req.ip);
   res.send(req.headers, req.data, req.ip, req.router, req.body);
+});
+app.get("/nukes", function (req, res) {
+  try {
+    res.json({
+      isNukesActive: true
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      code: err.code,
+      message: err.message
+    });
+  }
 });
 app.listen(process.env.PORT || port, function () {
   console.log("Server running at http://".concat(ip.address(), ":").concat(port));
--- server.js
+++ server.js
@@ -7,36 +7,40 @@ const verify = require("./routes/verify");
 const ip = require("ip");
 require("dotenv").config();
 
-
 require("dotenv/config");
 const bodyParser = require("body-parser");
 
 const app = express();
 const port = 8080;
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true}, () => {
-    console.log("Connected to MongoDB");
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, () => {
+  console.log("Connected to MongoDB");
 });
 
-app.use(express.static("./images"))
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.static("./images"));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
-app.use('/event', event);
-app.use('/user', user);
-app.use('/verify', verify);
-
+app.use("/event", event);
+app.use("/user", user);
 
 app.get("/", (req, res) => {
-    console.log(req.url);
-    console.log(req.headers);
-    console.log(req.body);
-    console.log(req.ip);
-    res.send(req.headers, req.data, req.ip, req.router, req.body);
+  console.log(req.url);
+  console.log(req.headers);
+  console.log(req.body);
+  console.log(req.ip);
+  res.send(req.headers, req.data, req.ip, req.router, req.body);
 });
-
+app.get("/nukes", (req, res) => {
+  try {
+    res.json({ isNukesActive: true });
+  } catch (err) {
+    console.log(err);
+    res.json({ code: err.code, message: err.message });
+  }
+});
 app.listen(process.env.PORT || port, () => {
-    console.log(`Server running at http://${ip.address()}:${port}`);
+  console.log(`Server running at http://${ip.address()}:${port}`);
 });
 
 module.exports = app;
